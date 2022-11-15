338d314c8830e89d2aeae84f58d33bd8ca64428b
André Monteiro <andre@dune.com>
2022-11-15 23:30:24 +0000

    Tweaks to data-mem module

--- data/cmdb-data-mem.mjs
+++ data/cmdb-data-mem.mjs
@@ -2,7 +2,7 @@ import crypto from "node:crypto";
 
 let groups = [
     {
-        id: "g1",
+        id: 1,
         name: "Action",
         description: "Action movies",
         movies: ["tt1", "tt2", "tt3", "tt4"],
@@ -11,7 +11,7 @@ let groups = [
 
     },
     {
-        id: "g2",
+        id: 2,
         name: "Drama",
         description: "Drama movies",
         movies: ["tt1", "tt2", "tt3"],
@@ -19,12 +19,28 @@ let groups = [
         userId: 2
     },
     {
-        id: "g3",
+        id: 3,
         name: "Comedy",
         description: "Comedy movies",
         movies: ["tt1", "tt2"],
         totalDuration: 317,
         userId: 3
+    },
+    {
+        id: 4,
+        name: "Sci-Fi",
+        description: "Sci-Fi movies",
+        movies: ["tt1", "tt2", "tt3", "tt4"],
+        totalDuration: 671,
+        userId: 1
+    },
+    {
+        id: 5,
+        name: "Horror",
+        description: "Horror movies",
+        movies: ["tt1",  "tt4"],
+        totalDuration: 671,
+        userId: 2
     }
 ]
 
@@ -32,19 +48,21 @@ let users = [
     {id: 1, userName: "Andre", token : "abc"},
     {id: 2, userName: "Monteiro", token : "zxc"},
 ]
-let nextUserId = 3
-
+let nextUserId = users.length + 1
 
 function getGroups() {
     return groups.length > 0 ? Promise.resolve(groups) : Promise.reject("No groups")
 }
 
-function getGroup(groupId) {
+async function getGroup(groupId) {
+    console.log(`Data: Getting group with id: ${groupId}`)
     const group = groups.find(group => group.id === groupId)
-    return group ? Promise.resolve(group): Promise.reject("Group not found")
+    console.log("Group: " + JSON.stringify(group, null, 2))
+    return group
 }
 
 function createUser(userName) {
+    // TODO: Add username from request
     const user= { id: nextUserId, userName: "User-" + nextUserId++, token: crypto.randomUUID() }
     users.push(user)
     return Promise.resolve(user)
