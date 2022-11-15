26d961262484444a44215dabe85e46d8500f411b
André Monteiro <andre@dune.com>
2022-11-15 23:32:08 +0000

    Services error codes generation refactor

--- errors.mjs
+++ errors.mjs
@@ -1,18 +1,41 @@
-const errors = [{
-    code: 1, message: 'Invalid token'
-}, {
-    code: 2, message: 'Group not found'
-}, {
-    code: 3, message: 'Access Denied'
-}, {
-    code: 4, message: 'Not Found',
-}, {
-    code: 5, message: 'Internal Server Error'
-}, {
-}]
+// Services Errors
+//
 
-
-export default function (code, message = null) {
-    this.code = code
-    this.message = message || errors[code].message
+export default {
+    INVALID_PARAMETER: argName => {
+        return {
+            code: 1,
+            message: `Invalid argument ${argName}`
+        }
+    },
+    GROUP_NOT_FOUND: (groupId) => {
+        return {
+            code: 2,
+            message: `Group with id ${groupId} not found`
+        }
+    },
+    USER_NOT_FOUND: (userId) => {
+        return {
+            code: 3,
+            message: `User with id ${userId} not found`
+        }
+    },
+    MOVIE_NOT_FOUND: (movieId) => {
+        return {
+            code: 4,
+            message: `Movie with id ${movieId} not found`
+        }
+    },
+    GROUP_ACCESS_DENIED: (groupId) => {
+        return {
+            code: 5,
+            message: `Access denied to ${groupId}`
+        }
+    },
+    UNKNOWN: () => {
+        return {
+            code: 6,
+            message: `Unknown error`
+        }
+    }
 }
