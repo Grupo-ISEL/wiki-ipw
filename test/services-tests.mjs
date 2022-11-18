98aefe0d66f2bfd6bf96da19d0de8b5b9c5b987e
Simao <92046226+SimaoCabral@users.noreply.github.com>
2022-11-18 00:43:29 +0000

    testsImplementationStart
    testData created so that comparisons are not hardCoded

--- test/services-tests.mjs
+++ test/services-tests.mjs
@@ -1,1 +1,19 @@
-// TODO: Add tests
+import servicesGroups from "../services/cmdb-services-groups.mjs";
+import {testData} from "./testsData.mjs";
+
+
+
+describe('cmdb-services-groups tests', function () {
+
+    const user1 = testData.users[0]
+
+    describe('handle token validation tests', function () {
+        const validTestToken = user1.token
+        it(`should return an array with group objects`,async function () {
+            // Act
+            let res = servicesGroups.getGroups(validTestToken)
+            // Assert
+            res.then(groups => groups.should.equal(testData.id1Groups))
+        })
+    })
+})
---
+++ test/testsData.mjs
@@ -0,0 +1,34 @@
+export const testData = {
+    users: [
+        {id: 1, name: "Andre", token: "abc"},
+        {id: 2, name: "Monteiro", token: "zxc"},
+    ],
+    id1Groups:[
+        {
+            "id": 1,
+            "name": "Action",
+            "description": "Action movies",
+            "movies": [
+                "tt1",
+                "tt2",
+                "tt3",
+                "tt4"
+            ],
+            "totalDuration": 671,
+            "userId": 1
+        },
+        {
+            "id": 4,
+            "name": "Sci-Fi",
+            "description": "Sci-Fi movies",
+            "movies": [
+                "tt1",
+                "tt2",
+                "tt3",
+                "tt4"
+            ],
+            "totalDuration": 671,
+            "userId": 1
+        }
+    ]
+}
